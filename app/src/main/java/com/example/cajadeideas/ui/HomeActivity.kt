3bbf0d41414515996b409d00e390caae9dabad90
miiguel_ro <miguelromeroorons@gmail.com>
2024-01-14 19:59:25 +0100

    initial commit

---
+++ app/src/main/java/com/example/cajadeideas/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cajadeideas.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cajadeideas.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
