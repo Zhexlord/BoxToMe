b0235982f6109fde6a3eb19dcd871d55fa68a9a4
rmg7757 <rmg7757@gmail.com>
2024-01-19 19:37:49 +0100

    Añadido NavComponent

--- app/src/main/java/com/example/cajadeideas/ui/HomeActivity.kt
+++ app/src/main/java/com/example/cajadeideas/ui/HomeActivity.kt
@@ -2,12 +2,16 @@ package com.example.cajadeideas.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
 import com.example.cajadeideas.R
+import com.example.cajadeideas.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        print("zuluEsGay")
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
