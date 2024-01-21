914b26da64eb4926d5cf47dc34831c6540f6f572
miiguel_ro <miguelromeroorons@gmail.com>
2024-01-21 16:42:39 +0100

    Error en el binding

--- app/src/main/java/com/example/cajadeideas/ui/HomeActivity.kt
+++ app/src/main/java/com/example/cajadeideas/ui/HomeActivity.kt
@@ -12,6 +12,5 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 }
